d54f6fffb9f667db1b596eae363433c102da3e59
Hendrik Bode <Honnigorega@googlemail.com>
2025-10-15 23:19:43 +0200

    simplified health component to only deal with health management and not listen to hitbox events as well

--- _scenes/components/health/health_component.gd
+++ _scenes/components/health/health_component.gd
@@ -3,23 +3,16 @@ extends Node
 
 signal death
 
+## Maximum health value
 @export var max_health: int = 1
-@export var hitbox_list: Array[Hitbox]
 
+## Current health value; Emits death signal on reaching 0 or below
 @onready var health := max_health:
 	set(value):
 		health = value
 		if health <= 0:
 			death.emit()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Check required components
-	assert(hitbox_list.size() > 0, "ERROR: You must provide at least 1 hitbox.")
-
-	for hitbox in hitbox_list:
-		hitbox.hit.connect(_on_hitbox_hit)
-
-func _on_hitbox_hit(area: Area2D) -> void:
-	if area.is_in_group("bullet"):
-		health -= area.bullet_component.damage
+## Reduce health by a specified value
+func reduce_health(value: int) -> void:
+	health -= value
